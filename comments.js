4b7c90083028de38d977e1802be2fc8071607a17
adithyaS-10688844 <165755479+adithyaS-10688844@users.noreply.github.com>
2024-04-15 12:49:51 +0000

    Copilot third commit

--- comments.js
+++ comments.js
@@ -1,14 +1,17 @@
 // Create Web Server
-// Create Web Server
-const express = require('express');
-const app = express();
-const port = 3000;
-
-app.get('/comments', (req, res) => {
-  res.send('Comments Page');
-});
-
-app.listen(port, () => {
-  console.log(`Web Server is running on port ${port}`);
-});
-// Path: index.js
+//
+// Dependencies:
+//   express
+//   body-parser
+//
+// Comments:
+//   This is a simple server to handle POST requests to the /comments endpoint.
+//   It will read the request body and log the content to the console.
+//   The server will run on port 3000.
+//
+// Usage:
+//   To run the server, use the following commands:
+//   $ node comments.js
+//   To test the server, use the following command:
+//   $ curl -X POST -H "Content-Type: application/json" -d '{"comment":"This is a test comment."}' http://localhost:3000/comments
+//
