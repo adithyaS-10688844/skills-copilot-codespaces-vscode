4a292874c0f5dc190a98ab09235b4e5045ff32d1
adithyaS-10688844 <165755479+adithyaS-10688844@users.noreply.github.com>
2024-04-15 12:47:16 +0000

    Copilot third commit

--- comments.js
+++ comments.js
@@ -1,49 +1,14 @@
-//Create Web Server
-var http = require('http');
-var fs = require('fs');
-var url = require('url');
-var qs = require('querystring');
-var comments = [];
-var server = http.createServer(function(req, res){
-    var parseUrl = url.parse(req.url, true);
-    var pathname = parseUrl.pathname;
-    if(pathname === '/'){
-        fs.readFile('./index.html', function(err, data){
-            if(err){
-                console.log(err);
-                res.end('Server Error');
-            }
-            res.writeHead(200, {'Content-Type': 'text/html'});
-            res.end(data);
-        });
-    }else if(pathname === '/comments'){
-        if(req.method === 'GET'){
-            res.writeHead(200, {'Content-Type': 'text/plain'});
-            var query = parseUrl.query;
-            var comment = query.comment;
-            if(comment){
-                comments.push(comment);
-            }
-            res.end(comments.join('\n'));
-        }else if(req.method === 'POST'){
-            var postData = '';
-            req.setEncoding('utf8');
-            req.on('data', function(chunk){
-                postData += chunk;
-            });
-            req.on('end', function(){
-                var query = qs.parse(postData);
-                var comment = query.comment;
-                comments.push(comment);
-                res.writeHead(200, {'Content-Type': 'text/plain'});
-                res.end(comments.join('\n'));
-            });
-        }
-    }else{
-        res.writeHead(404, {'Content-Type': 'text/plain'});
-        res.end('Not Found');
-    }
+// Create Web Server
+// Create Web Server
+const express = require('express');
+const app = express();
+const port = 3000;
+
+app.get('/comments', (req, res) => {
+  res.send('Comments Page');
 });
-server.listen(3000,() =>{
-    console.log("running on the port 3000")
+
+app.listen(port, () => {
+  console.log(`Web Server is running on port ${port}`);
 });
+// Path: index.js
